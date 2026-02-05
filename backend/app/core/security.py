aefb1ec6fa118eab3d303cabbe2f63f5ad024cad
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-05 14:22:07 +0300

    core security final code

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -10,3 +10,14 @@ def hash_password(password: str):
 
 def verify_password(password: str, hashed: str):
     return pwd_context.verify(password, hashed)
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.access_token_expire_minutes
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(
+        to_encode,
+        settings.secret_key,
+        algorithm=settings.algorithm
+    )
